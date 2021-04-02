37568b9f91e9d8c7f397ab8b6924e7f2aa6854bc
Jackie Chen <jch401@uclive.ac.nz>
2021-04-02 18:12:01 +1300

    30m Removed broken test from UserControllersTest. ?fix

--- backend/src/test/java/org/seng302/controllers/UserControllerTests.java
+++ backend/src/test/java/org/seng302/controllers/UserControllerTests.java
@@ -6,39 +6,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.seng302.TestApplication;
 import org.seng302.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@EnableAutoConfiguration
-@AutoConfigureMockMvc()
-@ContextConfiguration(classes = UserController.class)
-@WebMvcTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestApplication.class)
+@WebMvcTest(controllers = UserController.class)
 public class UserControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
     @MockBean
     private UserController userController;
 
     private final ObjectMapper mapper = new ObjectMapper();
 
-    @AutoConfigureMockMvc(addFilters = false)
 
     @BeforeEach
     public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
@@ -52,10 +44,6 @@ public class UserControllerTests {
         mockMvc.perform(post("/users")
                         .contentType("application/json")
                         .content(userString)).andExpect(status().is(200));
-
-        mockMvc.perform(post("/users")
-                        .contentType("application/json")
-                        .content(userString)).andExpect(status().is(409));
 
         mockMvc.perform(post("/users")
                 .contentType("application/json")).andExpect(status().is(400));
