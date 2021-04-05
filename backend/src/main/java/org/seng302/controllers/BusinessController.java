26a89c9e93d17dff2d6b080e5f2b76f73f506363
Jackie Chen <jch401@uclive.ac.nz>
2021-04-05 15:13:01 +1200

    S302T900-70 #time 2h 10m implemented endpoints for business admin rights (& associated tests). ?implement ?test
    - Changed the list of businessesAdministered to a list from a set, so the order of admins is consistent.
    - Partly changed how the GET endpoints return the users - returns a string of business administered instead of the id. This is to be fixed/fully completed to match api.
    - Also changed how our session check api call is returned - now returns a response instead of console log.

--- backend/src/main/java/org/seng302/controllers/BusinessController.java
+++ backend/src/main/java/org/seng302/controllers/BusinessController.java
@@ -3,9 +3,12 @@ package org.seng302.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.seng302.models.Business;
+import org.seng302.models.Role;
 import org.seng302.models.User;
 import org.seng302.models.requests.NewBusinessRequest;
+import org.seng302.models.requests.UserIdRequest;
 import org.seng302.repositories.BusinessRepository;
+import org.seng302.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,11 +25,14 @@ public class BusinessController {
     @Autowired
     private BusinessRepository businessRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     /**
      * Get request mapping for getting business by id
      * @param id the business's id
      * @return ResponseEntity
-     * @throws JsonProcessingException
+     * @throws JsonProcessingException when json mapping object to a json string fails unexpectedly.
      */
     @GetMapping("/businesses/{id}")
     public ResponseEntity<String> getBusiness(@PathVariable String id) throws JsonProcessingException {
@@ -68,4 +74,64 @@ public class BusinessController {
     public boolean isValidBusiness(Business business) {
         return (business.getName() != null && business.getDescription() != null && business.getBusinessType() != null);
     }
+
+    /**
+     * A PUT request that makes a user an administrator of a given business.
+     * @param userIdRequest DTO containing the userId to make admin of business.
+     * @param id the unique business Id.
+     * @param session current authenticated login session of the user.
+     * @return a response entity with the appropriate status code.
+     */
+    @PutMapping("/businesses/{id}/makeAdministrator")
+    public ResponseEntity<String> makeUserAdministrator(@RequestBody UserIdRequest userIdRequest, @PathVariable long id, HttpSession session) {
+        long userId = userIdRequest.getUserId();
+        User owner = (User) session.getAttribute("user");
+
+        Business business = businessRepository.findBusinessById(id);
+
+        if (business == null) { // 406 business non-existent.
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+        }
+
+        // 403 if user making request is not primary admin or DGAA.
+        if (!business.getPrimaryAdministrator().getId().equals(owner.getId()) && !owner.getRole().equals(Role.DGAA)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        User userToAdmin = userRepository.getOne(userId);
+        if (userToAdmin == null || business.getAdministrators().contains(userToAdmin)) { // 400 if user is non-existent or is already admin.
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        business.getAdministrators().add(userToAdmin);
+        businessRepository.save(business);
+        return ResponseEntity.status(HttpStatus.OK).build(); // 200
+    }
+
+    @PutMapping("/businesses/{id}/removeAdministrator")
+    public ResponseEntity<String> removeUserAdministrator(@RequestBody UserIdRequest userIdRequest, @PathVariable long id, HttpSession session) {
+        long userId = userIdRequest.getUserId();
+        User currentUser = (User) session.getAttribute("user");
+        Business business = businessRepository.findBusinessById(id);
+
+        if (business == null) { // 406 business non-existent.
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+        }
+
+        // 403 if user making request is not primary admin or DGAA.
+        if (!business.getPrimaryAdministrator().getId().equals(currentUser.getId()) && !currentUser.getRole().equals(Role.DGAA)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        User userToRevoke = userRepository.getOne(userId);
+        // 400 if user is non-existent, is not an admin, or is the primary admin.
+        if (userToRevoke == null || !business.getAdministrators().contains(userToRevoke) || business.getPrimaryAdministrator().getId().equals(userToRevoke.getId())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        business.getAdministrators().remove(userToRevoke);
+        businessRepository.save(business);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
 }
--- backend/src/main/java/org/seng302/utilities/WebSecurityConfig.java
+++ backend/src/main/java/org/seng302/utilities/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     /**
      * Configuration of web security - sets the rules on what user can go where.
      * @param http security configurer.
-     * @throws Exception
+     * @throws Exception when configuration fails.
      */
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -29,10 +29,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/checksession").authenticated()
+                .antMatchers("/checksession").permitAll()
                 .antMatchers(HttpMethod.POST, "/login", "/users").permitAll()
                 .antMatchers(HttpMethod.GET, "/users/search*").authenticated()
                 .antMatchers(HttpMethod.PUT, "/users/{id}/makeAdmin").hasRole("DGAA")
+                .antMatchers(HttpMethod.PUT, "/businesses/{id}/*").authenticated()
                 .anyRequest().permitAll()
 
                 .and()
