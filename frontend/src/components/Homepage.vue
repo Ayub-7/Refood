81ce4267a1eaf22077c0a7092611b464472152ce
Lachlan Reynolds <lar90@uclive.ac.nz>
2021-04-08 13:43:52 +1200

    S302T900-7 #time 1h Homepage redesign for mobile ?implement
    Reworking page design to be usable on mobile devices

--- frontend/src/components/Homepage.vue
+++ frontend/src/components/Homepage.vue
@@ -1,18 +1,10 @@
 <template>
   <!-- -->
   <div id="body" v-if="this.$store.state.userId != null">
-  <div class="main">
-      <h1 id="welcomeHeader">
-       <span id="pageTitle"> Welcome, {{this.userFirstName}}! </span>
-       <span id="profileLink" @click='goToProfilePage()' style='cursor: pointer'>Profile Page</span>
-      </h1>
-      <!-- Example Content class -->
-      <div class="content">
-          <!-- Example post or story on home page -->
-          <h2 class="contentHeader">Header</h2>
-          <p class="contentBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet sem magna. Nullam vestibulum condimentum laoreet. Integer et velit semper, blandit tellus ut, viverra ipsum. Aenean egestas dictum maximus. Curabitur egestas neque ex, eget laoreet ex blandit vel. Nullam ut augue nec odio hendrerit luctus accumsan in erat. Donec viverra congue laoreet. Mauris non eleifend ex. Pellentesque scelerisque sodales augue in sodales. Aenean sed tellus a enim mollis mollis. Nunc lobortis eros sit amet ligula mollis placerat.</p>
+      <div id="welcomeHeader">
+       <h1 id="pageTitle"> Welcome, {{this.userFirstName}}! </h1>
+       <h1 id="profileLink" @click='goToProfilePage()' style='cursor: pointer'>Profile Page</h1>
       </div>
-  </div>
   </div>
 </template>
 
@@ -52,59 +44,40 @@ export default Homepage;
 
 <style scoped>
 #body {
-  background-color: #F3EBF6;
+  display: grid;
+  /* background-color: #F3EBF6; */
   font-family: 'Ubuntu', sans-serif;
-
+  padding: 3em;
 }
 
 #welcomeHeader {
-  background-color: #E1BEE7;
-  border-radius: 0.5em;
-  padding: 10px;
+  display: grid;
+  grid-template-columns: 80% 1fr;
+  text-align: center;
+  background-color: #F3EBF6;
 }
 
 #pageTitle {
-  margin-left: 25%;
+  border-radius: 10px;
+  background-color: #F3EBF6;
+  font-size: 60px
+
 }
 
 #profileLink {
-  font-size: 80%;
-  padding: 5px;
-  background: #f6cffc;
-  float: right;
-  border-radius: 0.5em;
-  box-shadow: 1px 3px 7px #6e666f;
+  border-radius: 10px;
+  background-color: #f3e3f9;
+  margin-left: 5px;
+  font-size: 30px;
+  padding-top: 5%;
+  box-shadow: 0px 3px 8px#cfcfcf;
 }
 
 
-.content {
-  text-align: center;
-  padding-top: 5em;
-
-}
-
-.contentHeader {
-  background: #E1BEE7;
-  margin-left: 100px;
-  margin-right: 100px;
-  border-radius: 0.5em;
-}
-
-.contentBody {
-  font-size: 15px;
-  margin-left: 2em;
-  margin-right: 2em;
-}
 
 
-.main {
-  background-color: #FFFFFF;
-  width: 1000px;
-  height: 1000px;
-  margin: 7em auto;
-  border-radius: 1.5em;
-  box-shadow: 0px 11px 35px 2px rgba(0, 0, 0, 0.14);
-}
+
+
 
 a {
   text-shadow: 0px 0px 3px rgba(117, 117, 117, 0.12);
