847b41bce89833b4c3f39f86d94dd66742e6c60b
Lachlan Reynolds <lar90@uclive.ac.nz>
2021-04-12 14:10:00 +1200

    S302T900-76 #time 30m fixed some merge issues like businesses data not being shown ?fix ?chore

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -118,8 +118,8 @@ const app = {
   beforeMount() {
     api.checkSession()
     .then((response) => {
-      console.log(response)
       mutations.setUserLoggedIn(response.data.id, response.data.role);
+      mutations.setUserPrimaryBusinesses(response.data.businessesAdministered);
     })
   },
 };
