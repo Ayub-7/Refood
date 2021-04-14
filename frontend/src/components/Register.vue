67d23e9c83a0653d717e3c0d435cd80beec3ba2b
Omar Sheta <osh18@uclive.ac.nz>
2021-04-14 12:43:30 +1200

    S302T900-79 #time 1m Fixing merge conflicts ?fix

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -281,7 +281,7 @@ const Register = {
      * Sets address field when address in suggestions is clicked
      */
     setAddress: function(address) {
-      this.homeaddressObj = address
+      this.homeaddressObj = address;
     },
 
     setAddressText: function(addressString) {
