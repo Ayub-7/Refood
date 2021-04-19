57a6e0d75cd6133e6d5750064dbfc70b1baba1da
Omar Sheta <osh18@uclive.ac.nz>
2021-04-19 23:32:34 +1200

    S302T900-91 #time 45m I have successfully managed to get Cucumber set up and it runs the "failing" tests. But that is normal as the steps are not defined, but will be defined sometime later. ?test ?chore

--- backend/src/test/java/org/seng302/TestApplication.java
+++ backend/src/test/java/org/seng302/TestApplication.java
@@ -1,16 +1,12 @@
 package org.seng302;
 
-import io.cucumber.java.Before;
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Sole purpose of this class is to exist to make JPA repository tests work.
  */
 @SpringBootApplication
 public class TestApplication {
-    @Before
-    public void setup_cucumber_spring_context(){
-        // Dummy method so cucumber will recognize this class as glue
-        // and use its context configuration.
-    }
 }
