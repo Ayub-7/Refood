1bd0b94bd3b963c5976f253d84f7c94162c054e7
Kye Oldham <kno42@uclive.ac.nz>
2021-05-11 23:16:33 +1200

    S302T900-165 #time 1h 42m Created inventory entity by creating a model class in the backend. Had to join with the Product entitys composite key. Also created new inventory request class and the corresponding method for Inventory.java ?implement

---
+++ backend/src/main/java/org/seng302/models/Inventory.java
@@ -0,0 +1,80 @@
+package org.seng302.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.seng302.models.requests.NewInventoryRequest;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Entity class that holds the information of a businesss product inventory.
+ */
+@Data
+@Entity
+public class Inventory {
+
+    // Composite key of product id & business id.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumns({
+            @JoinColumn(name="product_id"),
+            @JoinColumn(name="business_id")
+    })
+    private Product product;
+
+    private int quantity;
+    private double pricePerItem;
+    private double totalPrice;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date manufacturedDate;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date sellByDate;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date bestBeforeDate;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date expiryDate;
+
+    protected Inventory() { }
+
+    public Inventory(long id, Product product, int quantity, double pricePerItem, double totalPrice, Date manufacturedDate, Date sellByDate, Date bestBeforeDate, Date expiryDate) {
+        this.id = id;
+        this.product = product;
+        this.quantity = quantity;
+        this.pricePerItem = pricePerItem;
+        this.totalPrice = totalPrice;
+        this.manufacturedDate = manufacturedDate;
+        this.sellByDate = sellByDate;
+        this.bestBeforeDate = this.bestBeforeDate;
+        this.expiryDate = this.expiryDate;
+    }
+
+    /**
+     * Used for when a new product request is called.
+     * @param newInventoryRequest The request body information that was mapped into a NewInventoryRequest.
+     * @param businessId business to assign the product inventory rights to.
+     */
+    public Inventory(NewInventoryRequest newInventoryRequest, Long businessId, Product product) {
+        this.product = product;
+        this.quantity = newInventoryRequest.getQuantity();
+        this.pricePerItem = newInventoryRequest.getPricePerItem();
+        this.totalPrice = newInventoryRequest.getTotalPrice();
+        this.manufacturedDate = newInventoryRequest.getManufacturedDate();
+        this.sellByDate = newInventoryRequest.getSellByDate();
+        this.bestBeforeDate = newInventoryRequest.getBestBeforeDate();
+        this.manufacturedDate = newInventoryRequest.getManufacturedDate();
+        this.expiryDate = newInventoryRequest.getExpiryDate();
+        //this.created = new Date();
+    }
+
+}
---
+++ backend/src/main/java/org/seng302/models/requests/NewInventoryRequest.java
@@ -0,0 +1,29 @@
+package org.seng302.models.requests;
+
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class NewInventoryRequest {
+
+    private int quantity;
+    private double pricePerItem;
+    private double totalPrice;
+    private Date manufacturedDate;
+    private Date sellByDate;
+    private Date bestBeforeDate;
+    private Date expiryDate;
+
+
+    public NewInventoryRequest(int quantity, double pricePerItem, double totalPrice, Date manufacturedDate, Date sellByDate, Date bestBeforeDate, Date expiryDate) {
+        this.quantity = quantity;
+        this.pricePerItem = pricePerItem;
+        this.totalPrice = totalPrice;
+        this.manufacturedDate = manufacturedDate;
+        this.sellByDate = sellByDate;
+        this.bestBeforeDate = bestBeforeDate;
+        this.manufacturedDate = manufacturedDate;
+        this.expiryDate = expiryDate;
+        //this.created = new Date();
+    }
+}
