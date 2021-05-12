33d000b136c8a5c85d492e3b99c738fbdedc9dae
Lachlan Reynolds <lar90@uclive.ac.nz>
2021-05-12 20:43:22 +1200

    Oof left g next to import and build failed ?fix

--- backend/src/main/java/org/seng302/controllers/InventoryController.java
+++ backend/src/main/java/org/seng302/controllers/InventoryController.java
@@ -8,7 +8,7 @@ import org.seng302.repositories.BusinessRepository;
 import org.seng302.repositories.InventoryRepository;
 import org.seng302.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;g
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
