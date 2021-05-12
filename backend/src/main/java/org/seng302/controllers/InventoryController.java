523b5f6c6688b8f4d40e97f78d435c7ec75ed215
Lachlan Reynolds <lar90@uclive.ac.nz>
2021-05-12 20:40:42 +1200

    S302T900-188 #time 2h Setup basic post request, spent time trying to find issue with repository, still need to add validation to request object ?implement

--- backend/src/main/java/org/seng302/controllers/InventoryController.java
+++ backend/src/main/java/org/seng302/controllers/InventoryController.java
@@ -2,19 +2,15 @@ package org.seng302.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.seng302.models.Business;
-import org.seng302.models.Product;
-import org.seng302.models.Role;
-import org.seng302.models.User;
+import org.seng302.models.*;
+import org.seng302.models.requests.NewInventoryRequest;
 import org.seng302.repositories.BusinessRepository;
+import org.seng302.repositories.InventoryRepository;
 import org.seng302.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;g
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -31,6 +27,9 @@ public class InventoryController {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
 
     /**
      * Get request mapping for getting business inventory by business id
@@ -55,5 +54,28 @@ public class InventoryController {
                 return ResponseEntity.status(HttpStatus.OK).body(products);
             }
         }
+    }
+
+
+    @PostMapping("/businesses/{id}/inventory")
+    public ResponseEntity<List<Product>> postInventory(@PathVariable Long id, @RequestBody NewInventoryRequest request, HttpSession session) {
+        Business business = businessRepository.findBusinessById(id);
+        if(business == null) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+        } else {
+            ArrayList adminIds = business.getAdministrators().stream().map(User::getId).collect(Collectors.toCollection(ArrayList::new));
+            User currentUser = (User) session.getAttribute(User.USER_SESSION_ATTRIBUTE);
+
+            if (!(adminIds.contains(currentUser.getId()) || Role.isGlobalApplicationAdmin(currentUser.getRole()))) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            } else {
+                Inventory newInventoryItem = new Inventory(request, business.getId());
+                inventoryRepository.save(newInventoryItem);
+
+                return ResponseEntity.status(HttpStatus.CREATED).build();
+            }
+        }
+
+
     }
 }
