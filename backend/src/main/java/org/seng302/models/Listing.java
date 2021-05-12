f50502172b7acb8da831f716f41f3dbb9c98039f
Jackie Chen <jch401@uclive.ac.nz>
2021-05-12 17:32:36 +1200

    S302T900-148 #time 25m Finished implementation of Listing entity. ?implement ?test ?document - Documented the Listing object class. - Added a basic constructor for the Listing object, as well as the empty constructor for JPA. - Tweaked documentation of a businessController endpoint to be more detailed about responses. - Also added a set of tests for them.

--- backend/src/main/java/org/seng302/models/Listing.java
+++ backend/src/main/java/org/seng302/models/Listing.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import javax.persistence.*;
 import java.util.Date;
 
+/**
+ * Entity class that represents a listing for the sale of some product from a business' inventory.
+ */
 @Data
 @Entity
 public class Listing {
@@ -24,4 +27,26 @@ public class Listing {
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date closes;
 
+    /**
+     * Constructor a new Listing object. Id not included.
+     * @param inventoryItem item that this Listing object will represent.
+     * @param quantity the number of items associated with this Listing.
+     * @param price the value of the Listing.
+     * @param moreInfo an (optional) field for businesses to provide further info about the listing.
+     * @param created date and t ime when the listing was created.
+     * @param closes date and time when the listing closes.
+     */
+    public Listing(Inventory inventoryItem, int quantity, double price, String moreInfo, Date created, Date closes) {
+        this.inventoryItem = inventoryItem;
+        this.quantity = quantity;
+        this.price = price;
+        this.moreInfo = moreInfo;
+        this.created = created;
+        this.closes = closes;
+    }
+
+    /**
+     * Empty constructor for JPA use.
+     */
+    protected Listing() {}
 }
