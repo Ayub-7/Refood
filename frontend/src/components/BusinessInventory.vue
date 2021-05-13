60a5fa3e8fe770da3b74080114ccbbb577427fcf
Omar <osh18@uclive.ac.nz>
2021-05-13 22:02:46 +1200

    S302T900-157 #time 15m Beginning to implement the add to inventory form, as a modal as my team prefers that form ?implement

--- frontend/src/components/BusinessInventory.vue
+++ frontend/src/components/BusinessInventory.vue
@@ -5,7 +5,33 @@
       <div id="title"> Inventory </div>
       <div id="header-buttongroup">
         <vs-button class="header-button" @click="$router.push(`/businesses/${$route.params.id}/products`)">Product Catalogue</vs-button>
-        <vs-button class="header-button">New Inventory Listing</vs-button>
+        <vs-button @click="popupActivo2=true" class="header-button">New Inventory Listing</vs-button>
+        <!-- TEST -->
+        <vs-popup classContent="popup-example"  title="Lorem ipsum dolor sit amet" :active.sync="popupActivo2">
+          <vs-input class="inputx" placeholder="Placeholder" v-model="value1"/>
+          <vs-input disabled class="inputx" placeholder="Disabled" v-model="value2"/>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+          </p>
+          <vs-select
+              label="Figuras"
+              v-model="select1"
+          >
+            <vs-select-item :key="index" :value="item.value" :text="item.text" v-for="item,index in options1" />
+          </vs-select>
+          <vs-button @click="popupActivo3=true" color="primary" type="filled">Open Inner Popup</vs-button>
+          <vs-popup title="Inner popup" :active.sync="popupActivo3">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <br><br>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <br><br>
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </vs-popup>
+        </vs-popup>
+        <!-- TEST -->
       </div>
     </div>
 
@@ -53,15 +79,24 @@
 <script>
 export default {
   name: "BusinessInventory",
-
-  data: function() {
+  data(){
     return {
       inventory: [],
-
+      select1:3,
+      options1:[
+        {text:'IT',value:0},
+        {text:'Blade Runner',value:2},
+        {text:'Thor Ragnarok',value:3},
+      ],
+      value1:'',
+      value2:'',
+      popupActivo2:false,
+      popupActivo3:false
     }
-  },
+  }
 }
 </script>
+
 
 <style scoped>
   #container {
