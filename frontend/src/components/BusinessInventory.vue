19108bcfc4537e07e57051c4e3c03c2478a3f125
Jackie Chen <jch401@uclive.ac.nz>
2021-05-13 23:33:38 +1200

    S302T900-156 #time 1h 25m WIP implementation of new item listing modal. ?implement
    - vs-prompt is a type of dialog, allows for custom content in dialog.
    - Had difficulties getting it to work, mainly cos i used ':active-sync' instead of ':active.sync'...

--- frontend/src/components/BusinessInventory.vue
+++ frontend/src/components/BusinessInventory.vue
@@ -5,9 +5,20 @@
       <div id="title"> Inventory </div>
       <div id="header-buttongroup">
         <vs-button class="header-button" @click="$router.push(`/businesses/${$route.params.id}/products`)">Product Catalogue</vs-button>
-        <vs-button class="header-button">New Inventory Listing</vs-button>
+        <vs-button class="header-button">New Inventory Product</vs-button>
+        <vs-button @click="newListingPopup = true" class="header-button">New Item Listing</vs-button> <!-- todo: remove this when testing done -->
       </div>
     </div>
+
+    <vs-prompt :active.sync="newListingPopup" title="Add a new listing">
+      <div>
+        <label>Qty</label><vs-input-number v-model="qty"></vs-input-number> <!-- todo: max based on current inventory amount -->
+        <vs-input class="new-listing-input" v-model="qty" label-placeholder="Qty"></vs-input>
+        <vs-input class="new-listing-input" v-model="price" label-placeholder="Price"></vs-input>
+        <vs-input class="new-listing-input" v-model="moreInfo" label-placeholder="More Info"></vs-input>
+        <vs-input class="new-listing-input" v-model="closes" label-placeholder="Close Date"></vs-input>
+      </div>
+    </vs-prompt>
 
     <vs-divider></vs-divider>
     <!-- Table View -->
@@ -46,7 +57,6 @@
         </vs-tr>
       </template>
     </vs-table>
-
   </vs-card>
 </template>
 
@@ -58,8 +68,16 @@ export default {
     return {
       inventory: [],
 
+      newListingPopup: false,
+      qty: 0,
+      price: 0.00,
+      moreInfo: "",
+      created: "",
+      closes: "",
     }
   },
+
+
 }
 </script>
 
@@ -90,6 +108,12 @@ export default {
     margin: 0 1em;
   }
 
+  /* ===== NEW LISTING DIALOG/PROMPT ====== */
+  .new-listing-input {
+    padding: 1em 0;
+  }
+
+
   /* ===== INVENTORY TABLE ===== */
   .table-head {
     border-radius: 1em;
@@ -97,6 +121,16 @@ export default {
 
   #table {
     margin: 1em;
+  }
+
+  @media screen and (max-width: 850px) {
+    #header-container {
+      flex-direction: column;
+    }
+
+    #title {
+      padding: 0.5em 0 1em 0.5em;
+    }
   }
 
 </style>
