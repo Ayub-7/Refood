3ece5e389a87095aaf072efb0c98de04522fecee
Ronan Avery <rta114@uclive.ac.nz>
2021-05-17 10:41:54 +1200

    S302T900-146 #time 2m Staging fix ?fix

--- runner/staging-backend.sh
+++ runner/staging-backend.sh
@@ -2,5 +2,5 @@
 
 # Run the staging backend server
 
-fuser -k 9090/tcp || true
-java -jar staging-backend/libs/backend-0.0.1-SNAPSHOT.jar --server.port=9090 --spring.profiles.active=dev
+fuser -k 9499/tcp || true
+java -jar staging-backend/libs/backend-0.0.1-SNAPSHOT.jar --server.port=9499 --spring.profiles.active=staging
