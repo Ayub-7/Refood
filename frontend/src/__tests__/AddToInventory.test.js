239e53e1ee3bffae076f7e92cdb0b5dc18d430dc
Kye Oldham <kno42@uclive.ac.nz>
2021-05-20 21:49:25 +1200

    S302T900-51 #time 15m added jest test files, to be implemented by omar

---
+++ frontend/src/__tests__/AddToInventory.test.js
@@ -0,0 +1,72 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import AddToInventory from '../components/AddToInventory';
+import Vuesax from 'vuesax';
+
+
+const mockInventory = [
+    {
+        "id": 2,
+        "product": {
+            "id": "W04GP5EC0B1798680",
+            "name": "Compound - Mocha",
+            "description": "vel ipsum praesent blandit lacinia erat vestibulum sed magna at nunc",
+            "manufacturer": "Nestle",
+            "recommendedRetailPrice": 88.93,
+            "created": "2021-01-11 20:54:46",
+            "images": [],
+            "primaryImagePath": null
+        },
+        "quantity": 7,
+        "pricePerItem": 3,
+        "totalPrice": 80,
+        "manufactured": "2020-01-27",
+        "sellBy": null,
+        "bestBefore": "2021-08-27",
+        "expires": "2021-08-27",
+        "productName": "Compound - Mocha",
+        "productId": "W04GP5EC0B1798680"
+    }
+]
+
+
+const localVue = createLocalVue();
+localVue.use(Vuesax);
+
+let wrapper;
+
+let $route = {
+    params: {
+        id: 1,
+    }
+}
+
+let $log = {
+    debug: jest.fn(),
+}
+
+beforeEach(() => {
+    wrapper = shallowMount(AddToInventory, {
+        mocks: {$route, $log},
+        stubs: {},
+        methods: {},
+        localVue,
+    })
+
+    const getBusinessInventory = jest.spyOn(BusinessInventory.methods, "getBusinessInventory");
+
+    getBusinessInventory.mockResolvedValue([]);
+
+
+
+
+});
+
+afterEach(() => {
+    wrapper.destroy();
+});
+
+describe('Component', () => {
+    test('is a Vue instance', () => {
+        expect(wrapper.isVueInstance).toBeTruthy();
+    });
+});
