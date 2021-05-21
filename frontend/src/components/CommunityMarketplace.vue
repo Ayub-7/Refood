75dc3f781db7d70ed7f002728f1e6b0b01897c88
Lachlan Reynolds <lar90@uclive.ac.nz>
2021-05-21 21:27:30 +1200

    S302T900-155 #time 1h 30m Styling and made into component ?implement -Turned grid view into component because it was being reused 3 times -Added a little test data so could see how it looks

--- frontend/src/components/CommunityMarketplace.vue
+++ frontend/src/components/CommunityMarketplace.vue
@@ -2,69 +2,89 @@
   <div>
     <vs-tabs alignment="center">
       <vs-tab label="For Sale">
-        <vs-card id="cardContainer">
-        <div>
-          <vs-row id="marketRow">
-            <vs-col id="marketCard" type="flex" vs-justify="center" vs-align="center" vs-lg="3" vs-sm="12" v-for="card in testData" :key="card.id">
-              <div style="margin: 10px;">
-                <vs-card actionable :fixed-height="true">
-                  <div slot="media" id="cardHeader">
-                    <img id="marketImage" :src="require('../../public/ProductShoot.jpg')" />
-                  </div>
-                  <div>
-                    <div id="cardTitle">{{card.title}}</div>
-                    <div id="cardDescription">{{card.description}}</div>
-                  </div>
-                </vs-card>
-              </div>
-            </vs-col>
-          </vs-row>
-        </div>
-        </vs-card>
+        <MarketplaceGrid :cardData="testData" />
       </vs-tab>
-      <vs-tab label="Wanted"><div></div></vs-tab>
-      <vs-tab label="Exchange"><div></div></vs-tab>
+      <vs-tab label="Wanted">
+        <div>
+          <MarketplaceGrid :cardData="testData.slice(1, 4)" />
+        </div>
+      </vs-tab>
+      <vs-tab label="Exchange">
+        <div>
+          <MarketplaceGrid :cardData="testData.slice(1,2)" />
+        </div>
+      </vs-tab>
     </vs-tabs>
   </div>
 </template>
 
 <script>
+import MarketplaceGrid from './MarketplaceGrid.vue'
 export default {
   name: "CommunityMarketplace",
+  components: {MarketplaceGrid},
 
   data: () => {
     return {
       testData: [
         {
           id: 1,
-          title: 'Marketplace Item 1',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum ut mi et tempor. Morbi nec molestie eros. Donec ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum ut mi et tempor. Morbi nec molestie eros. Donec ornare.'
+          title: 'Beans',
+          description: 'I need to get rid of these beans someone please buy them $1000 ono' ,
+          keywords: [
+            {
+              id: 1,
+              name: 'beans'
+              }
+          ]
         },
         {
           id: 2,
-          title: 'Marketplace Item 2',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum ut mi et tempor. Morbi nec molestie eros. Donec ornare.'
+          title: 'Waste Food',
+          description: 'We have a lot of waste food that needs to be sold ',
+          keywords: [
+            {
+              id: 2,
+              name: 'waste'
+              }
+          ]
         },
         {
           id: 3,
-          title: 'Marketplace Item 3',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum ut mi et tempor. Morbi nec molestie eros. Donec ornare.'
+          title: 'Silvia s14 SR20DET',
+          description: '21k flat no cheaper first in first serve S14 Facelift •SR20DET (TURBO) •Body has done 199xxxkm •Engine had a rebuild done at 197xxxkm •New reconditioned TURBO  •Has current WOF REG AND CERT  •Lowered on adjustable suspension •On mags Any questions please ask',
+          keywords: [
+            {
+              id: 3,
+              name: 'carr'
+              },
+                     {
+              id: 4,
+              name: 'vroom'
+              }
+          ]
         },
+
         {
           id: 4,
-          title: 'Marketplace Item 1',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum ut mi et tempor. Morbi nec molestie eros. Donec ornare.'
+          title: 'Bad Rats: the Rats\' Revenge Steam Key',
+          description: 'Bad Rats: The Rats\' Revenge is a 2009 puzzle video game developed by Invent4 Entertainment. Over a string of levels, the player places a set of rats and static objects to guide a ball towards a trap that kills a cat',
+          keywords: [
+            {
+              id: 3,
+              name: 'rats'
+              },
+                     {
+              id: 4,
+              name: 'game'
+              },
+              {
+              id: 5,
+              name: 'steam'
+              }
+          ]
         },
-        {
-          id: 5,
-          title: 'Marketplace Item 2',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum ut mi et tempor. Morbi nec molestie eros. Donec ornare.'
-        },
-        {
-          id: 6,
-          title: 'Marketplace Item 3',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum ut mi et tempor. Morbi nec molestie eros. Donec ornare.'
-        }
+        
       ]
     }
   }
@@ -78,27 +98,17 @@ vs-tab {
   color: #1F74FF;
 }
 
-#marketImage {
-  width: 100%;
-  height: auto;
+/* REMOVE AUTO SCROLL HIDING, SO USER KNOWS IF PARAGRAPH IS LONGER THAN CARD SIZE */
+
+::-webkit-scrollbar {
+  -webkit-appearance: none;
+  width: 5px;
 }
 
-#marketCard {
-  margin: 0px;
-}
-
-#cardTitle {
-  font-weight: bold;
-  font-size: 17px;
-}
-
-#cardContainer {
-  padding: 15px;
-  width: 90%;
-  margin: auto;
-}
-
-#marketRow {
+::-webkit-scrollbar-thumb {
+  border-radius: 4px;
+  background-color: rgba(0, 0, 0, .5);
+  box-shadow: 0 0 1px rgba(255, 255, 255, .5);
 }
 
 
---
+++ frontend/src/components/MarketplaceGrid.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+    <vs-card id="cardContainer">
+        <div>
+          <vs-row id="marketRow">
+            <!-- Change vs-lg to 2 if you want 6 per row or 3 if you want 4 per row -->
+            <vs-col id="marketCard" type="flex" vs-justify="center" vs-align="center" vs-lg="3" vs-sm="12" v-for="card in cardData" :key="card.id">
+              <div style="margin: 10px; width: 90%;">
+                <vs-card actionable>
+                  <div slot="media" id="cardHeader">
+                    <img id="marketImage" :src="require('../../public/ProductShoot.jpg')" />
+                  </div>
+                  <div>
+                    <div id="cardTitle">{{card.title}}</div>
+                    <!-- Need to add limit or something to description -->
+                    <div id="cardDescription">{{card.description}}</div>
+                    <vs-row>
+                      <div id="keywordWrapper">
+                      <vs-col type="flex" v-for="keyword in card.keywords" :key="keyword.id" vs-w="3">
+                        <div id="cardKeywords">#{{keyword.name}}</div>
+                      </vs-col>
+                      </div>
+                    </vs-row>
+                  </div>
+                </vs-card>
+              </div>
+            </vs-col>
+          </vs-row>
+        </div>
+    </vs-card>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['cardData']
+
+}
+</script>
+
+<style>
+
+/* CARD STYLING */
+
+#marketImage {
+  width: 100%;
+  height: auto;
+}
+
+#cardTitle {
+  font-weight: bold;
+  font-size: 17px;
+}
+
+#cardContainer {
+  padding: 15px;
+  width: 90%;
+  margin: auto;
+}
+
+#cardTitle {
+  height: 40px;
+}
+
+#cardDescription {
+  margin-top: 10px;
+  height: 120px;
+  overflow-y: scroll;
+}
+
+#cardKeywords {
+  color: #1F74FF;
+  font-size: 15px;
+}
+
+#keywordWrapper {
+  margin-top: 10px;
+  height:50px;
+  width: 100%;
+  overflow-y: scroll;
+}
+
+
+</style>
