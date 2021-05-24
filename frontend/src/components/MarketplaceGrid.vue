c72b7e7defb842b4866299ade73de822b709bc2a
Kye Oldham <kno42@uclive.ac.nz>
2021-05-24 18:35:39 +1200

    S302T900-159 #time 11m fixed pathing in Marketplace grid and table components so jest tests would work ?fix ?test

--- frontend/src/components/MarketplaceGrid.vue
+++ frontend/src/components/MarketplaceGrid.vue
@@ -9,7 +9,7 @@
                 <vs-card actionable>
                   <div slot="media" id="cardHeader">
                     <!-- Default image for now -->
-                    <img id="marketImage" :src="require('../../public/ProductShoot.jpg')" />
+                    <img id="marketImage" src="ProductShoot.jpg" />
                   </div>
                   <div>
                     <div id="cardTitle">{{card.title}}</div>
