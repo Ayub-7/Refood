d2ff0564437e96408dbc1fd4bbd5e5b20a59d3f2
Omar Sheta <osh18@uclive.ac.nz>
2021-07-05 14:54:57 +1200

    S302T900-235 #time 30m Wrote a cucumber test for card extension ?test ?implement

--- backend/src/test/java/org/seng302/team900/steps/UCM4.java
+++ backend/src/test/java/org/seng302/team900/steps/UCM4.java
@@ -1,44 +1,68 @@
 package org.seng302.team900.steps;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.mockito.InjectMocks;
-import org.mockito.Mockito;
 import org.seng302.controllers.CardController;
+import org.seng302.controllers.UserController;
+import org.seng302.finders.UserFinder;
+import org.seng302.models.Address;
+import org.seng302.models.Card;
+import org.seng302.models.MarketplaceSection;
+import org.seng302.models.User;
+import org.seng302.models.requests.LoginRequest;
+import org.seng302.models.requests.NewCardRequest;
 import org.seng302.repositories.CardRepository;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.seng302.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import javax.servlet.http.HttpSession;
+import javax.xml.bind.ValidationException;
+import java.security.NoSuchAlgorithmException;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.util.Date;
-import java.util.TimeZone;
 
-@WebMvcTest(controllers = CardController.class)
+import static java.time.Instant.ofEpochMilli;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class UCM4 {
-
-    @InjectMocks
-    private CardController cardController;
-
-    @MockBean
-    private CardRepository cardRepository;
+    private User testUser;
+    private NewCardRequest cardRequest;
+    private Card card;
+    private Address addr;
+    private Date firstDate;
+    private Date secondDate;
 
     @Given("An existing card has just expired")
-    public void anExistingCardHasJustExpired() throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        sdf.setTimeZone(TimeZone.getTimeZone("NZST"));
-        Date d = new Date(); //current date
-        Date dateBefore = new Date(d.getTime() - 14 * 24 * 3600 * 1000);
-        Date NOW = sdf.parse(dateBefore.toString());
-        Mockito.when(Date.class).thenReturn(NOW);
+    public void anExistingCardHasJustExpired() throws NoSuchAlgorithmException, ValidationException {
+        addr = new Address(null, null, null, null, null, "Australia", "12345");
+        testUser = new User("Rayna", "YEP", "Dalgety", "", "" , "rdalgety3@ocn.ne.jp","2006-03-30","+7 684 622 5902",new Address("32", "Little Fleur Trail", "Christchurch" ,"Canterbury", "New Zealand", "8080"),"ATQWJM");
+        testUser.setId(1);
+        cardRequest = new NewCardRequest(testUser.getId(), "Card Title", "Desc", "Test, Two", MarketplaceSection.FORSALE);
+        card = new Card(cardRequest, testUser);
+        card.setDisplayPeriodEnd(new Date());
     }
 
     @When("I choose to extend it")
     public void iChooseToExtendIt() {
+        firstDate = card.getDisplayPeriodEnd();
+        card.updateDisplayPeriodEndDate();
+        secondDate = card.getDisplayPeriodEnd();
     }
 
     @Then("The expiry date for the card is extended and the card is still there")
     public void theExpiryDateForTheCardIsExtendedAndTheCardIsStillThere() {
+        assertThat(firstDate).isNotEqualTo(secondDate);
     }
 }
