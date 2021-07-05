9e72333366dbabdab46923a09af6ddbf8971bdc6
Kye Oldham <kno42@uclive.ac.nz>
2021-07-05 22:24:50 +1200

    S302T900-227 #time 4m removed unused imports and ran code through sonarlint ?chore

--- backend/src/main/java/org/seng302/repositories/CardRepository.java
+++ backend/src/main/java/org/seng302/repositories/CardRepository.java
@@ -4,10 +4,6 @@ import org.seng302.models.Card;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import org.springframework.transaction.annotation.Transactional;
 import org.seng302.models.*;
 
