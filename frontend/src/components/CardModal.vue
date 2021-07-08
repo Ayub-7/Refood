d307765a4a8585259233a8315b033b22023303f9
Danila Bublik <dbu59@uclive.ac.nz>
2021-07-08 12:54:36 +1200

    S302T900-307 #time 1h added styling to the modal, fixed the date showing as timestamp and added a message button ?implement

--- frontend/src/components/CardModal.vue
+++ frontend/src/components/CardModal.vue
@@ -1,12 +1,22 @@
 <template>
-  <vs-popup :title="selectedCard.title" :active.sync="showing">
-    <div><b>Description:</b> {{selectedCard.description}}</div>
-    <div><b>Creator name:</b> {{selectedCard.user.firstName}} {{selectedCard.user.lastName}}</div>
-    <div><b>Creation date:</b> {{selectedCard.created}}</div>
-    <div><b>Expiry date:</b> {{selectedCard.displayPeriodEnd}}</div>
-    <div><b>Location:</b> {{selectedCard.user.homeAddress.suburb}} {{selectedCard.user.homeAddress.city}} {{selectedCard.user.homeAddress.country}}</div>
-    <div><b>Keywords:</b> {{selectedCard.keywords}}</div>
-    <div><b>Section:</b> {{selectedCard.section}}</div>
+  <vs-popup id="card-modal" :title="selectedCard.title" :active.sync="showing">
+    <div id="card-modal-name"><vs-icon icon="face" class="inline"></vs-icon>
+      <p class="inline text">{{selectedCard.user.firstName}} {{selectedCard.user.lastName}}</p>
+    </div>
+    <div id="card-modal-location"><vs-icon icon="location_on" class="inline"></vs-icon>
+      <p class="inline text">{{selectedCard.user.homeAddress.suburb}} {{selectedCard.user.homeAddress.city}} {{selectedCard.user.homeAddress.country}}</p>
+    </div>
+    <div id="card-modal-closing"><vs-icon icon="event"></vs-icon>
+      <p class="inline text">Closes on {{toStringDate(selectedCard.displayPeriodEnd)}}</p>
+    </div>
+    <div id="card-modal-description">{{selectedCard.description}}</div>
+    <div id="card-modal-keywords" v-for="keyword in selectedCard.keywords.split(' ')" :key="keyword" >#{{keyword}}</div>
+    <vs-divider></vs-divider>
+    <div id="card-modal-bottom">
+      <div id="card-modal-listed">Listed: {{toStringDate(selectedCard.created)}}</div>
+      <vs-button id="card-modal-message-button">Message</vs-button>
+    </div>
+
   </vs-popup>
 </template>
 
@@ -30,10 +40,49 @@ export default {
         /**
          * Converts seconds to date
          */
+        toStringDate: function (timestamp) {
+          const dateFull = new Date(timestamp);
+          return dateFull.toDateString();
+        }
 
       }
 }
 </script>
 
 <style scoped>
+
+.inline {
+  display: inline-block;
+  position: relative;
+}
+
+.text {
+  top: -5px;
+  left: 5px;
+  font-size: large;
+}
+
+#card-modal-keywords {
+  color: #1F74FF;
+  font-size: 15px;
+  padding: 2px;
+  float: left;
+}
+
+#card-modal-bottom {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#card-modal-listed {
+  position: relative;
+  flex: 50%;
+  font-size: large;
+  top: 7px;
+}
+
+#card-modal-message-button {
+  flex: 0%;
+}
+
 </style>
