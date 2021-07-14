440f2204e09a89ad697fdfb8292cc4a04717c446
Sam Taylor <sta193@uclive.ac.nz>
2021-07-14 17:01:31 +1200

    S302T900-311 #time 1h Marketplace table shows all card entries. Created MarketpalceCommon.js to use functions between grid and table views ?implement

--- frontend/src/components/MarketplaceGrid.vue
+++ frontend/src/components/MarketplaceGrid.vue
@@ -9,18 +9,17 @@
                 <vs-card>
                   <div>
                     <div id="cardCreationDate">{{card.created}}</div>
-
                     <div id="cardUserName" v-if="card.user.firstName">{{card.user.firstName+" "+card.user.lastName}}</div>
-                    <div id="cardUserAddress" v-if="card.user.homeAddress">{{getGeneralAddress(card.user.homeAddress)}}</div>
+                    <div id="cardUserAddress" v-if="card.user.homeAddress">{{MarketpalceCommon.getGeneralAddress(card.user.homeAddress)}}</div>
 
 
                     <div id="cardTitle">{{card.title}}</div>
                     <!-- Need to add limit or something to description -->
                     <div id="cardDescription">{{card.description}}</div>
                     <!-- Keyword display -->
-                      <div v-if="card.keywords" id="keywordWrapper">
-                        <div id="cardKeywords"  v-for="keyword in getKeywords(card.keywords)" :key="keyword.id" >#{{keyword.name}}</div>
-                      </div>
+                    <div v-if="card.keywords" id="keywordWrapper">
+                      <div id="cardKeywords"  v-for="keyword in MarketpalceCommon.getKeywords(card.keywords)" :key="keyword.id" >#{{keyword.name}}</div>
+                    </div>
                   </div>
                 </vs-card>
               </div>
@@ -33,15 +32,15 @@
 
 <script>
 import CardModal from './CardModal.vue'
-
-import api from "../Api";
+import MarketpalceCommon from "./MarketpalceCommon.js";
 
 export default {
   props: ['cardData'],
   data: function () {
     return {
       cards: this.cardData,
-      selectedCard: null
+      selectedCard: null,
+      MarketpalceCommon
     }
   },
   components: {
@@ -49,11 +48,10 @@ export default {
   },
   watch: {
     "cardData": function(val) {
-      this.cards = this.checkCardList(val);
+      this.cards = MarketpalceCommon.checkCardList(val);
     }
   },
   methods: {
-
     /**
      * Method for opening card modal, calls method in child component to open modal
      */
@@ -61,75 +59,6 @@ export default {
       this.selectedCard = card;
       this.$refs.cardModal.openModal();
     },
-    /**
-     * Converts the space separated keywords to a JSON object recognized by the keywordWrapper
-     *
-     * @param keywords  space separated keywords
-     * @returns {[]}    JSON object
-     */
-
-    getKeywords: function(keywords) {
-      keywords = keywords.trimEnd().split(/\s+/);
-      let tmpKeywords = [];
-
-      for(let i=0;i<keywords.length;i++) {
-        let keyword = {};
-        keyword.key = i;
-        keyword.name = keywords[i];
-        tmpKeywords.push(keyword);
-      }
-      return tmpKeywords;
-    },
-
-    /**
-     * Translates the user's address into a general address string, listing only the country, city and suburb.\
-     *
-     * @param homeAddress   User's home address
-     * @returns String      General address string
-     */
-
-    getGeneralAddress: function (homeAddress) {
-      let addressStr = "";
-      if(homeAddress.country) {
-        addressStr += homeAddress.country;
-      }
-
-      if(homeAddress.city) {
-        addressStr += ", " + homeAddress.city;
-      }
-
-      if(homeAddress.suburb) {
-        addressStr += ", " + homeAddress.suburb;
-      }
-
-      return addressStr
-    },
-
-    /**
-     * Checks the data integrity of the list of cards
-     * ie every card includes a user
-     * if not, it will get one from the database and update the card list
-     *
-     * assumes the user id attribute is not null.
-     *
-     */
-    checkCardList: function(cards) {
-      cards.forEach(function (card, index) {
-        if (!card.user.firstName) {
-          api.getUserFromID(card.user)
-              .then((response) => {
-                cards[index].user = response.data;
-              }).catch((err) => {
-            if (err.response.status === 406) {
-              this.$vs.notify({title:'User not found: '+card.user, text:'This user does not exist.', color:'danger'});
-            }
-            throw new Error(`Error trying to get user info from id: ${err}`);
-          });
-
-        }
-      });
-      return cards;
-    }
   },
 }
 
