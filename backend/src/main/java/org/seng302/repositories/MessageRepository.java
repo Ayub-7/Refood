118746356acdb53637e8d0249cc4c9b6d454a427
Lachlan Reynolds <lar90@uclive.ac.nz>
2021-07-19 14:38:46 +1200

    S302T900-281 #time 40m Fixing issue with merge that Ayub encountered ?chore ?fix

--- backend/src/main/java/org/seng302/repositories/MessageRepository.java
+++ backend/src/main/java/org/seng302/repositories/MessageRepository.java
@@ -1,6 +1,7 @@
 package org.seng302.repositories;
 
 import org.seng302.models.Message;
+import org.seng302.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -10,8 +11,13 @@ import java.util.List;
 @RepositoryRestResource
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-    // I'm not actually sure if this works.
-    List<Message> getAllByCardUserId(long id);
+    /**
+     * Get Message by it's ID
+     *
+     * @param receiver receiver of messages we want to grab
+     * @return list of users messages
+     */
+    List<Message> findMessageByReceiver(User receiver);
 
     /**
      * Get Message by it's ID
