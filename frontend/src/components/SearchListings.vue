d0f715ab1d26a595b9876a5087333b5ff92c21a7
Kye Oldham <kno42@uclive.ac.nz>
2021-08-11 16:05:57 +1200

    S302T900-246 #time 38m fixed issues from code review ?fix

--- frontend/src/components/SearchListings.vue
+++ frontend/src/components/SearchListings.vue
@@ -1,28 +1,16 @@
 <template>
   <vs-card class="main">
-      <div class="profile-text-inner">
         <div id="header-container">
           <div id="page-title">
             <vs-icon icon="local_offer"/>
             Sales Listings</div>
           </div>
-        </div>
         <vs-divider style="padding: 4px;"></vs-divider>
 
         <div id="catalogue-options">
-          <vs-input class="search-input" type="search" placeholder="Enter a listing..." name="searchbarUser" v-model="searchbarListings" style="width: 400px; font-size: 24px" size="large"/>
+          <vs-input class="search-input" type="search" placeholder="Enter a listing..." name="searchbarUser" v-model="searchbarListings" style="width: 400px; font-size: 24px" size="medium"/>
           <vs-button class="header-button" >Search</vs-button>
 
-          <!-- If search query returns more than 10 products then this should be active -->
-<!--          <div id="grid-pagination">-->
-<!--            <div class="displaying">Displaying {{ searchRange[0] }}-{{ searchRange[1] }} of-->
-<!--              {{ filteredListings.length }}</div>-->
-<!--            <div v-if="filteredListings.length > 10" style="display: flex;">-->
-<!--              <div v-if="filteredListings.length > productsPerPage" style="display: flex;">-->
-<!--                <vs-pagination v-model="currentPage" :total="Math.round(listings.length/productsPerPage +0.4)"/>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
         </div>
 
         <vs-divider style="padding: 4px;"></vs-divider>
@@ -35,38 +23,34 @@
                 <h3 class="filter-label">
                   Business Type:
                 </h3>
-                <select v-model="businessType">
+                <vs-select v-model="businessType">
                   <option disabled value="">Please select one</option>
-                  <option value="name">Product Name</option>
+                  <option value="name">listings Name</option>
                   <option value="country">Country</option>
                   <option value="recommendedRetailPrice">Recommended Retail Price</option>
                   <option value="expiryDate">Expiry Date</option>
-                </select>
+                </vs-select>
                 </div>
-<!--                Add select with business types-->
                 <div class="vert-row">
                 <h3 class="filter-label">
                   Business Name:
                 </h3>
                 <vs-input class="filter-input" type="search" placeholder="Business name.." name="searchbarUser"  style="width: 400px; font-size: 24px" size="medium"/>
-                <!--                Add select with business types-->
               </div>
               </div>
-              <div class="parameter" id="product">
+              <div class="parameter" id="listings">
                 <div class="vert-row">
                 <h3 class="filter-label">
-                  Product Name:
+                  Prouduct Name:
                 </h3>
-                <vs-input class="filter-input" type="search" placeholder="Product name.." name="searchbarUser"  style="width: 400px; font-size: 24px" size="small"/>
-                <!--                Add select with business types-->
+                <vs-input class="filter-input" type="search" placeholder="Product name.." name="searchbarUser"  style="width: 400px; font-size: 24px" size="medium"/>
               </div>
               <div class="vert-row">
                 <h3 class="filter-label">
                   Price range:
                 </h3>
-                <vs-input class="price-input" type="search" placeholder="Min" name="searchbarUser"  style="width: 400px; font-size: 24px" size="small"/>
-                <vs-input class="price-input" type="search" placeholder="Max" name="searchbarUser"  style="width: 400px; font-size: 24px" size="small"/>
-                <!--                Add select with business types-->
+                <vs-input class="price-input" type="search" placeholder="Min" name="searchbarUser"  style="width: 400px; font-size: 24px" size="medium"/>
+                <vs-input class="price-input" type="search" placeholder="Max" name="searchbarUser"  style="width: 400px; font-size: 24px" size="medium"/>
               </div>
               </div>
 
@@ -76,14 +60,12 @@
                   Location:
                 </h3>
                 <vs-input class="filter-input" type="search" placeholder="Country.." name="searchbarUser"  style="width: 400px; font-size: 24px" size="medium"/>
-                <!--                Add select with business types-->
               </div>
                 <div class="vert-row">
                 <h3 class="filter-label">
                   Closing Date:
                 </h3>
                 <vs-input class="filter-input" type="date" name="searchbarUser"  style="width: 400px; font-size: 24px"/>
-                <!--                Add select with business types-->
               </div>
             </div>
             </div>
@@ -92,14 +74,14 @@
               <div>
                 <h3 class="filter-label" style="margin: auto; padding-right: 4px;">Sort By </h3>
                 <div>
-                <select v-model="selected">
+                <vs-select v-model="selected">
                   <option disabled value="">Please select one</option>
-                  <option value="name">Product Name</option>
+                  <option value="name">listings Name</option>
                   <option value="country">Country</option>
                   <option value="recommendedRetailPrice">Recommended Retail Price</option>
                   <option value="expiryDate">Expiry Date</option>
-                </select>
-                <vs-button class="sort-btn" @click="sortByName(null, selected, 0);" style="width: 100px">Sort</vs-button>
+                </vs-select>
+                <vs-button class="sort-btn"  style="width: 100px">Sort</vs-button>
                 </div>
                 </div>
             </div>
@@ -107,11 +89,10 @@
           <vs-divider style="padding: 4px;"></vs-divider>
           <div class="grid-container" style="margin: auto">
             <vs-card class="listing-card" v-for="listing in listings" :key="listing.id" :fixed-height="true">
-
               <div slot="media">
                 <ReImage :imagePath="listing.inventoryItem.product.primaryImagePath"></ReImage>
               </div>
-              <div style="margin: 2px 4px; font-size: 14px; font-weight: bold">{{ listing.productName }}</div>
+              <div style="margin: 2px 4px; font-size: 14px; font-weight: bold">{{ listing.listingsName }}</div>
               <div style="font-size: 14px; padding-left: 4px; margin: auto 0;">
                 <div>{{ currencySymbol }}{{ listing.price }}</div>
                 <div>{{ listing.quantity }}x</div>
@@ -126,7 +107,6 @@
               </div>
             </vs-card>
             <div class="title-centre">
-              <vs-pagination v-model="currentPage" :total="Math.round(listings.length/productsPerPage +0.4)"/>
             </div>
           </div>
         </div>
@@ -552,26 +532,18 @@ const SearchListings = {
         }
       ],
       currentPage: 1,
-      productsPerPage: 12,
       searchbarListings: "",
       businessType: "",
       errors: [],
       toggle: [1,1,1,1,1],
       filteredListings: [],
       enableTable: false,
-      productSearchIndexMin: 0,
-      productSearchIndexMax: 12,
       displaytype: true,
       currencySymbol: "",
       selected: "",
     };
   },
 
-  /**
-   * api.getBusinessProducts() queries the test back-end (json-server)
-   * at /businesses/${businessId}/products which returns a JSON object list of test products which can
-   * be filtered by the webpage.
- */
   mounted() {
     api.checkSession()
       .then((response) => {
@@ -595,118 +567,7 @@ const SearchListings = {
         console.log("Error with getting cities from REST Countries." + err);
       });
     },
-
-    /**
-     * Filters the displayed products alphabetically by
-     * @param event
-     * @param JSONField is the name of the field to sort by (as string)
-     */
-    sortByName: function (event, JSONField) {
-
-      const indexarray = ["id", "name", "description", "recommendedRetailPrice", "created"];
-
-      //toggles the classlist (arrow up or down) in the child DOM element: <i/>
-      if(event) {
-        if(event.target.firstElementChild) {
-          event.target.firstElementChild.classList.toggle('fa-angle-double-down');
-          event.target.firstElementChild.classList.toggle('fa-angle-double-up');
-        }
-      }
-
-      if (this.filteredListings) {
-        this.filteredListings.sort(function(a, b) {
-          var aField = a[JSONField];
-          var bField = b[JSONField];
-
-          //first check if null
-          if(aField == null) {
-            return 1;
-          }
-          if(bField == null) {
-            return -1;
-          }
-
-          //check if a or b contains any numbers or whitespace
-          //before capitalzation to avoid errors
-          //also check if boolean
-          if (!(typeof aField === "boolean" || typeof bField === "boolean")) {
-            if ( !(/[^a-zA-Z]/.test(aField) && (/[^a-zA-Z]/.test(bField))) ) {
-              aField = aField.toUpperCase();
-              bField = bField.toUpperCase();
-            }
-          }
-
-
-          //first < second
-          if (aField > bField ) {
-            return 1;
-          }
-          //second < first
-          if (aField< bField) {
-            return -1;
-          }
-          // a must be equal to b
-          return 0;
-        });
-
-        let index = indexarray.indexOf(JSONField);
-
-        if (index >= 0) {
-          if (this.toggle[index]) {
-            this.filteredListings.reverse();
-            this.toggle[index]=0;
-          } else {
-            this.toggle[index]=1;
-          }
-        }
-      }
-    },
-
-    /**
-     * Helper function to control the number of search range values.
-     * It increments Minimum and Maximum search index by 10 as long as the maximum search index does not exceed
-     * the number of filtered products.
-     */
-    increaseSearchRange: function () {
-      //Stop value from going over range
-      if(this.productSearchIndexMax < this.filteredListings.length) {
-        //console.log(this.productSearchIndexMax, this.filteredproducts.length, this.productSearchIndexMin)
-        let minMaxDiff = this.productSearchIndexMax - this.productSearchIndexMin;
-        this.productSearchIndexMin += minMaxDiff;
-        this.productSearchIndexMax += minMaxDiff;
-      }
-    },
-
-    /**
-     * Helper function to control the number of search range values.
-     * It decrements Minimum and Maximum search index by 10 as long as the minimum is at least 10.
-     */
-    decreaseSearchRange: function () {
-      //Stop value from reaching negative
-      if(this.productSearchIndexMin >= 1) {
-        let minMaxDiff = this.productSearchIndexMax - this.productSearchIndexMin;
-        this.productSearchIndexMin -= minMaxDiff;
-        this.productSearchIndexMax -= minMaxDiff;
-      }
-    },
   },
-
-  computed: {
-    /**
-     * Computes ranges to be displayed below table, max of range is switched
-     * to length of product query if the length is less than the current range
-     * @returns {Array} Array with start index and end index for searchRange
-     */
-    searchRange: function () {
-      let max = this.productSearchIndexMax;
-
-      if (max > this.filteredProducts.length) {
-        max = this.filteredProducts.length
-      }
-
-      return [this.productSearchIndexMin + 1, max]
-    }
-  }
 }
 
 export default SearchListings;
@@ -897,6 +758,9 @@ div#search-parameters {
   width: 70px !important;
 }
 
+.con-vs-card.fixedHeight {
+  height: auto;
+}
 
 @media screen and (max-width: 900px) {
   #catalogue-options {
