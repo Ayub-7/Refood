fa4f2bcaa4a57e627782d91ec3ae54c4d71fc860
Lachlan Reynolds <lar90@uclive.ac.nz>
2021-08-11 20:29:45 +1200

    S302T900-368 #time 2h Almost implemented ?implement - Added images, with button to go through each - Added all required info - Altered layout as required info was not taken into account - Need to fix image sizing and then test then should be done

--- frontend/src/components/ListingDetail.vue
+++ frontend/src/components/ListingDetail.vue
@@ -1,24 +1,34 @@
 <template>
   <vs-card id="listing-detail-container">
-
     <vs-row>
       <!-- Image area -->
-      <vs-col vs-w="8" id="image-area">
-        <div id="image-container">
-          <ReImage id="image"></ReImage>
+      <vs-col vs-w="6" id="image-area">
+        <div id="listing-image-container">
+          <ReImage id="listing-image" :imagePath="currentImage"></ReImage>
         </div>
+                  <vs-button @click="previousImage(currentImage)">
+            <vs-icon icon="arrow_back"></vs-icon>
+          </vs-button>
+
+          <vs-button @click="nextImage(currentImage)">
+            <vs-icon icon="arrow_forward"></vs-icon>
+          </vs-button>
       </vs-col>
       <!-- Listing details (closing date, business, etc) -->
-      <vs-col vs-w="4" id="listing-info-area">
+      <vs-col vs-w="6" id="listing-info-area">
         <div id="listing-info-container">
-          <div>Business: {{listing.inventoryItem.product.business.name}}</div>
-          <div>Price: {{listing.price}}</div>
+          <div id="business-name">Business: {{listing.inventoryItem.product.business.name}}</div>
+          <div> Price: {{currency.symbol}}{{listing.price}} {{currency.code}}</div>
           <div>Quantity: {{listing.quantity}}</div>
           <div>Closes: {{listing.created}}</div>
           <div>Created: {{listing.closes}}</div>
+          <div id="listing-moreInfo">{{listing.moreInfo}}</div>
           <div>Likes: {{listing.likes}}</div>
           <div>
-            <vs-button>Like listing</vs-button>
+            <vs-button class="listing-detail-btn">Like listing</vs-button>
+          </div>
+          <div>
+            <vs-button class="listing-detail-btn">Buy</vs-button>
           </div>
         </div>
       </vs-col>
@@ -26,11 +36,18 @@
 
     <vs-row>
       <vs-col vs-w="12">
-        <div id="listing-name">{{listing.inventoryItem.product.name}}</div> 
-        <div id="listing-moreInfo">{{listing.moreInfo}}</div>
+        <div id="product-info-area">
+          <div id="listing-name">{{listing.inventoryItem.product.name}}</div> 
+          <div>Manufacturer: {{listing.inventoryItem.product.manufacturer}} </div>
+          <div v-if="listing.inventoryItem.manufactured">Manufactured: {{listing.inventoryItem.manufactured}}</div>
+          <div v-if="listing.inventoryItem.sellBy">Sell By: {{listing.inventoryItem.sellBy}}</div>
+          <div v-if="listing.inventoryItem.bestBefore">Best Before: {{listing.inventoryItem.bestBefore}}</div>
+          <div v-if="listing.inventoryItem.expires">Expires: {{listing.inventoryItem.expires}}</div>
+          <div>{{listing.inventoryItem.product.description}}</div>
+        </div>
       </vs-col>
 
-      
+
     </vs-row>
     
   </vs-card>
@@ -40,6 +57,8 @@
 
 import api from "../Api";
 import ReImage from "../components/ReImage.vue";
+import axios from "axios";
+import { store } from "../store";
 export default {
   components: {
     ReImage
@@ -51,6 +70,9 @@ export default {
       businessId: null,
       listing: null,
       noBusiness: false,
+      currency: {symbol: '$', code: 'NZD'},
+      currentImage: null,
+      listingImages: []
     }
   },
 
@@ -59,6 +81,7 @@ export default {
     this.listingId = this.$route.params.listingId
 
     this.getBusinessListings(this.businessId, this.listingId);
+    this.setCurrency(store.userCountry);
   },
 
 
@@ -71,15 +94,34 @@ export default {
      */
     getBusinessListings(businessId, listingId) {
       api.getBusinessListings(businessId)
-      .then((response) => {
-        this.listing = this.filterListingFromListingsResponse(response.data, listingId)
-        console.log(this.listing)
-      }).catch((error) => {
-        if(error.response.status == 406) {
-          this.noBusiness = false;
-        }
-      })
+        .then((response) => {
+          console.log(response.data)
+          this.listing = this.filterListingFromListingsResponse(response.data, listingId)
+          this.listingImages = this.getListingImages(this.listing.inventoryItem.product.images)
+          this.currentImage = this.getPrimaryImage(this.listingImages, this.listing)
+        }).catch((error) => {
+          if(error.response.status == 406) {
+            this.noBusiness = false;
+          }
+        })
     },
+
+    /**
+     * Sets display currency based on the user's home country.
+     * @param country country for which currency is going to be retrieved
+     */
+    setCurrency(country) {
+      axios.get(`https://restcountries.eu/rest/v2/name/${country}`)
+        .then(response => {
+          console.log(response)
+          this.currency = {
+            symbol: response.data[0].currencies[0].symbol,
+            code: response.data[0].currencies[0].code
+          }
+        }).catch(err => {
+          this.$log.debug(err);
+        })
+      },
 
     /**
      * Filters response from API to only have listing with matching ID
@@ -92,9 +134,39 @@ export default {
     },
 
 
-    getListingImages() {
+    getListingImages(images) {
+      let productImages = [];
+      for (let image of images) {
+        console.log(image.fileName.split('/'));
+        let imagePathSplit = image.fileName.split('/')
+        let imagePath = imagePathSplit.splice(imagePathSplit.length - 2, 2).join('/')
+        productImages.push(imagePath)
+      }
+
+      return productImages;
+    },
+
+
+    getPrimaryImage(images, listing) {
+      return images.filter(imagePath => imagePath == listing.inventoryItem.product.primaryImagePath)[0]
+    },
+
+    nextImage(currentImage) {
+      console.log(this.listingImages)
+      let indexOfImage = this.listingImages.indexOf(currentImage);
+      console.log(indexOfImage)
+      this.currentImage = this.listingImages[(indexOfImage + 1) % this.listingImages.length]
+
+    },
+
+    previousImage(currentImage) {
+      console.log(this.listingImages)
+      let indexOfImage = this.listingImages.indexOf(currentImage);
+      console.log(indexOfImage)
+      this.currentImage = this.listingImages[(indexOfImage - 1) % this.listingImages.length]
 
     }
+
 
   }
 
@@ -105,18 +177,48 @@ export default {
 
 #listing-detail-container {
   height: 100%;
-  width: 70%;
+  width: 60%;
   margin: 1em auto;
 }
 
-#image-container {
-  width: 60%
+#listing-info-area {
+  text-align: center;
+  font-size: 15px;
 }
 
-#image > img {
-  max-width: 100%;
-  max-height: auto;
+
+#listing-image-container {
+  margin: auto;
+  width: 80%;
+  overflow: hidden;
 }
 
+#listing-image > img {
+  max-height: 300px;
+  min-height: 300px;
+  width: 100%;
+}
+
+#product-info-area {
+  margin-top: 3em;
+  text-align: center;
+  font-size: 15px;
+  padding-bottom: 2em;
+}
+
+#listing-name {
+  font-size: 25px;
+  margin-bottom: 10px;
+}
+
+#business-name {
+  font-size: 25px;
+  margin-bottom: 1em;
+}
+
+.listing-detail-btn {
+  margin-top: 5px;
+  width: 40%;
+}
 
 </style>
