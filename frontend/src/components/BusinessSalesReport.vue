6b8f909acb3e5c0367ca10debf4615786df1f80e
Jackie Chen <jch401@uclive.ac.nz>
2021-09-02 23:37:32 +1200

    S302T900-432 #time 1h 48m Finished implementation of page. ?implement
    - Page is 100% more responsive at all screen sizes
    - Added a 'go' button for dates.
    - Made the settings buttons 'toggleable'.
    - Other minor styling changes.
    - Still need to add the component to home page, add additional tooltips for better ux.

--- frontend/src/components/BusinessSalesReport.vue
+++ frontend/src/components/BusinessSalesReport.vue
@@ -10,22 +10,75 @@
       <!-- FILTER/GRANULARITY PICKER -->
       <vs-card id="options-container">
         <div class="options-header">Period</div>
-        <vs-button class="options-button" type="border">1 Day</vs-button>
-        <vs-button class="options-button" type="border">1 Week</vs-button>
-        <vs-button class="options-button" type="border">1 Month</vs-button>
-        <vs-button class="options-button" type="border">6 Months</vs-button>
-        <vs-button class="options-button" type="border">1 Year</vs-button>
-        <vs-button class="options-button" type="border">All</vs-button>
+        <vs-button v-bind:class="[{'active-button': activePeriodButton === '1-d'}, 'options-button']"
+                   type="border"
+                   style="grid-column: 1;"
+                   @click="onPeriodChange('1-d')">
+          1 Day
+        </vs-button>
+        <vs-button v-bind:class="[{'active-button': activePeriodButton === '1-w'}, 'options-button']"
+                   type="border"
+                   style="grid-column: 3;"
+                   @click="onPeriodChange('1-w')">
+          1 Week
+        </vs-button>
+        <vs-button v-bind:class="[{'active-button': activePeriodButton === '1-m'}, 'options-button']"
+                   type="border"
+                   style="grid-column: 1;"
+                   @click="onPeriodChange('1-m')">
+          1 Month
+        </vs-button>
+        <vs-button v-bind:class="[{'active-button': activePeriodButton === '6-m'}, 'options-button']"
+                   type="border"
+                   style="grid-column: 3;"
+                   @click="onPeriodChange('6-m')">
+          6 Months
+        </vs-button>
+        <vs-button v-bind:class="[{'active-button': activePeriodButton === '1-y'}, 'options-button']"
+                   type="border"
+                   style="grid-column: 1;"
+                   @click="onPeriodChange('1-y')">
+          1 Year
+        </vs-button>
+        <vs-button v-bind:class="[{'active-button': activePeriodButton === 'all'}, 'options-button']"
+                   type="border"
+                   style="grid-column: 3;"
+                   @click="onPeriodChange('all')">
+          All
+        </vs-button>
         <div class="options-header" style="font-size: 14px">Custom</div>
-        <vs-input type="date" size="small" class="date-input" label="Start" style="grid-row: 6"></vs-input>
-        <vs-input type="date" size="small" class="date-input" label="End" style="grid-row: 6"></vs-input>
+        <vs-input type="date" size="small" class="date-input" label="Start" style="grid-column: 1"/>
+        <p style="margin: auto auto 0">-</p>
+        <vs-input type="date" size="small" class="date-input" label="End" style="grid-column: 3"/>
+        <vs-button type="border" size="small" style="grid-column: 1/4; width: 100px; margin: auto;">Go</vs-button>
 
-        <vs-divider style="grid-column: 1/3;"/>
+        <vs-divider style="grid-column: 1/4; margin: 0 auto;"/>
+
         <div class="options-header">Granularity</div>
-        <vs-button class="options-button" type="border">Week</vs-button>
-        <vs-button class="options-button" type="border">Month</vs-button>
-        <vs-button class="options-button" type="border">Year</vs-button>
-        <vs-button class="options-button" type="border">All</vs-button>
+        <vs-button v-bind:class="[{'active-button': activeGranularityButton === 'w'}, 'options-button']"
+                   type="border"
+                   style="grid-column: 1;"
+                   @click="onGranularityChange('w')">
+          Week
+        </vs-button>
+        <vs-button v-bind:class="[{'active-button': activeGranularityButton === 'm'}, 'options-button']"
+                   type="border"
+                   style="grid-column: 3;"
+                   @click="onGranularityChange('m')">
+          Month
+        </vs-button>
+        <vs-button v-bind:class="[{'active-button': activeGranularityButton === 'y'}, 'options-button']"
+                   type="border"
+                   style="grid-column: 1;"
+                   @click="onGranularityChange('y')">
+          Year
+        </vs-button>
+        <vs-button v-bind:class="[{'active-button': activeGranularityButton === 'all'}, 'options-button']"
+                   type="border"
+                   style="grid-column: 3;"
+                   @click="onGranularityChange('all')">
+          All
+        </vs-button>
       </vs-card>
 
       <vs-card id="summary-container">
@@ -41,8 +94,6 @@
           <div class="summary-subheader">AVG SALE VALUE</div>
           <div>$58.92</div>
         </div>
-
-<!--          <vs-divider style="grid-column: 1/5;"/>-->
       </vs-card>
 
       <div id="stats-container">
@@ -50,55 +101,55 @@
           <div class="stat-subheader">Average Sale</div>
           <h2 style="padding-left: 12px">$56.45</h2>
           <div class="sub-header stat-change">
-            <vs-icon color="red" icon="arrow_drop_down" class="stat-change-icon"/>
+            <vs-icon color="red" icon="arrow_drop_down" class="stat-change-icon"/> <!-- decrease icon -->
             <div>12.34% from last year</div>
           </div>
-          <div class="sub-header" style="padding-left: 12px;">Jan 1 2020 - Dec 31 2020</div>
+          <div class="sub-header stat-date">Jan 1 2020 - Dec 31 2020</div>
         </div>
         <div class="stat-box">
           <div class="stat-subheader">Average Price Per Item</div>
           <h2 style="padding-left: 12px">$20.69</h2>
           <div class="sub-header stat-change">
-            <vs-icon color="green" icon="arrow_drop_up" class="stat-change-icon"/>
+            <vs-icon color="green" icon="arrow_drop_up" class="stat-change-icon"/> <!-- increase icon -->
             <div>12.34% from last year</div>
           </div>
-          <div class="sub-header" style="padding-left: 12px;">Jan 1 2020 - Dec 31 2020</div>
+          <div class="sub-header stat-date">Jan 1 2020 - Dec 31 2020</div>
         </div>
         <div class="stat-box">
-          <div class="stat-subheader">Average Sale</div>
+          <div class="stat-subheader">Average Items Per Sale</div>
           <h2 style="padding-left: 12px">$56.45</h2>
           <div class="sub-header stat-change">
             <vs-icon color="red" icon="arrow_drop_down" class="stat-change-icon"/>
             <div>12.34% from last year</div>
           </div>
-          <div class="sub-header" style="padding-left: 12px;">Jan 1 2020 - Dec 31 2020</div>
+          <div class="sub-header stat-date">Jan 1 2020 - Dec 31 2020</div>
         </div>
         <div class="stat-box">
-          <div class="stat-subheader">Average Sale</div>
+          <div class="stat-subheader">Total Sale Value</div>
+          <h2 style="padding-left: 12px">$564545.45</h2>
+          <div class="sub-header stat-change">
+            <vs-icon color="red" icon="arrow_drop_down" class="stat-change-icon"/>
+            <div>12.34% from last year</div>
+          </div>
+          <div class="sub-header stat-date">Jan 1 2020 - Dec 31 2020</div>
+        </div>
+        <div class="stat-box">
+          <div class="stat-subheader">Total Items Sold</div>
+          <h2 style="padding-left: 12px">696</h2>
+          <div class="sub-header stat-change">
+            <vs-icon color="red" icon="arrow_drop_down" class="stat-change-icon"/>
+            <div>12.34% from last year</div>
+          </div>
+          <div class="sub-header stat-date">Jan 1 2020 - Dec 31 2020</div>
+        </div>
+        <div class="stat-box">
+          <div class="stat-subheader">Total Sales</div>
           <h2 style="padding-left: 12px">$56.45</h2>
           <div class="sub-header stat-change">
             <vs-icon color="red" icon="arrow_drop_down" class="stat-change-icon"/>
             <div>12.34% from last year</div>
           </div>
-          <div class="sub-header" style="padding-left: 12px;">Jan 1 2020 - Dec 31 2020</div>
-        </div>
-        <div class="stat-box">
-          <div class="stat-subheader">Average Sale</div>
-          <h2 style="padding-left: 12px">$56.45</h2>
-          <div class="sub-header stat-change">
-            <vs-icon color="red" icon="arrow_drop_down" class="stat-change-icon"/>
-            <div>12.34% from last year</div>
-          </div>
-          <div class="sub-header" style="padding-left: 12px;">Jan 1 2020 - Dec 31 2020</div>
-        </div>
-        <div class="stat-box">
-          <div class="stat-subheader">Average Sale</div>
-          <h2 style="padding-left: 12px">$56.45</h2>
-          <div class="sub-header stat-change">
-            <vs-icon color="red" icon="arrow_drop_down" class="stat-change-icon"/>
-            <div>12.34% from last year</div>
-          </div>
-          <div class="sub-header" style="padding-left: 12px;">Jan 1 2020 - Dec 31 2020</div>
+          <div class="sub-header stat-date">Jan 1 2020 - Dec 31 2020</div>
         </div>
       </div>
     </div>
@@ -107,7 +158,35 @@
 
 <script>
 export default {
-  name: "BusinessSalesReport"
+  name: "BusinessSalesReport",
+
+  data: function() {
+    return {
+      // Used to determine which setting is currently selected - prevents re-clicking, and highlights the active button.
+      activePeriodButton: "",
+      activeGranularityButton: "",
+    }
+  },
+
+  methods: {
+
+    /**
+     * Something happens when this function is called. (todo: do something here).
+     * @param period string of the selected period.
+     */
+    onPeriodChange: function(period) {
+      this.activePeriodButton = period;// Changes the period button to be selected and disabled.
+    },
+
+    /**
+     * Something happens when this function is called. (todo: do something here).
+     * @param period string of the selected granularity.
+     */
+    onGranularityChange: function(period) {
+      this.activeGranularityButton = period; // Changes the granularity button to be selected and disabled.
+    }
+  }
+
 }
 </script>
 
@@ -132,24 +211,28 @@ export default {
 }
 
 #content-container {
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr 3fr auto;
+  grid-column-gap: 1em;
   justify-content: space-around;
+
 }
 
-/* OPTIONS CONTAINER */
+/* === OPTIONS CONTAINER === */
 #options-container {
-  max-width: 300px;
+  width: fit-content;
+  height: fit-content;
 }
 
 #options-container >>> .vs-card--content {
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 1em 4px;
+  grid-template-columns: 150px 4px 150px;
+  grid-gap: 1em 2px;
   padding-bottom: 16px;
 }
 
 .options-header {
-  grid-column: 1/3;
+  grid-column: 1/4;
   text-align: center;
   font-weight: 500;
   font-size: 18px;
@@ -160,20 +243,21 @@ export default {
   margin: auto;
 }
 
+.active-button {
+  background-color: rgb(31,116,255)!important;
+  color: white;
+  pointer-events: none;
+}
+
 .date-input {
-  width: 140px;
+  width: 136px;
   margin: auto;
 }
 
 /* MONTH/WEEK/YEAR REPORT CONTAINER */
 #summary-container {
-  /*border: black 1px solid;*/
-  max-width: 600px;
-  margin: 0 2em;
   overflow-y: scroll;
-}
-
-#summary-container >>> .vs-card--content {
+  height: 685px;
 }
 
 .row-summary-container {
@@ -206,6 +290,7 @@ export default {
   border: 1px solid rgba(0, 0, 0, 0.16);
   border-radius: 12px;
   height: fit-content;
+  width: fit-content;
   padding: 12px 12px 12px 0;
 
   display: flex;
@@ -226,6 +311,48 @@ export default {
   font-weight: bold;
   font-size: 12px;
   padding-left: 12px;
+}
+
+.stat-date {
+  padding-left: 12px;
+  font-size: 10px;
+}
+
+@media screen and (max-width: 1600px) {
+  #stats-container {
+    grid-template-columns: 1fr;
+  }
+}
+
+@media screen and (max-width: 1310px) {
+  #content-container {
+    grid-template-columns: auto 2fr;
+  }
+
+  #stats-container {
+    grid-column: 1/3;
+    grid-template-columns: repeat(auto-fit, 160px);
+    justify-content: space-around;
+  }
+}
+
+@media screen and (max-width: 975px) {
+  #content-container {
+    grid-template-columns: 1fr;
+    grid-row-gap: 1em;
+  }
+
+  #options-container {
+    margin: 0 auto;
+  }
+
+  #summary-container{
+    grid-column: 1;
+  }
+
+  #stats-container{
+    grid-column: 1;
+  }
 }
 
 </style>
