07504cc98f56b25f386e8e9ba906b3b5a3dfddc1
Omar <osh18@uclive.ac.nz>
2021-09-28 13:20:11 +1300

    S302T900-486 #time 2m final touch ups ?fix

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -92,7 +92,7 @@ const routes = [
   {path: '/businesses/:id', name: 'Business', component: Business},
   {path: '/businesses/:id/sales-history', name: 'BusinessSalesHistory', component: BusinessSalesHistory},
   {path: '/businesses/:businessId/listings/:listingId', name: 'Listing', component: ListingDetail},
-  {path: '/business/modify', name: 'ModifyBusiness', component: ModifyBusiness},
+  {path: '/businesses/:id/modify', name: 'ModifyBusiness', component: ModifyBusiness},
   {
     path: '*',
     name: 'catchAll',
