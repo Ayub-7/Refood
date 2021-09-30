0e195bb7720985376fc29957c38df162b0b44221
Lachlan Reynolds <lar90@uclive.ac.nz>
2021-09-30 14:14:54 +1300

    S302T900-516 #time 15m Merge and fixed issues with merge ?fix - Merged delete and put into my branch

--- backend/src/main/java/org/seng302/models/User.java
+++ backend/src/main/java/org/seng302/models/User.java
@@ -151,7 +151,8 @@ public class User implements Serializable {
         this.images.add(image);
     }
 
-    /**
+
+   /**
      * Deletes a users image and assigns a new primary
      * image if required
      * @param imageId
@@ -163,21 +164,6 @@ public class User implements Serializable {
                 this.images.remove(image);
                 removeImage = image;
                 break;
-
-    /**
-     * Sets the path of the thumbnail of the primary image
-     * @param path The path to the image
-     */
-    public void setPrimaryThumbnailPath(String path) {
-        this.primaryThumbnailPath = path;
-    }
-
-    public void updatePrimaryImage(long id, String imageId, String imageExtension) {
-        if (this.primaryImagePath == null) {
-            if (System.getProperty("os.name").startsWith("windows")) {
-                this.setPrimaryImage(String.format("business_%d\\%s%s", id, imageId, imageExtension));
-            } else {
-                this.setPrimaryImage(String.format("business_%d/%s%s", id, imageId, imageExtension));
             }
         }
         assert removeImage != null;
@@ -187,6 +173,19 @@ public class User implements Serializable {
             this.primaryImagePath = null;
         }
     }
+
+
+
+    public void updatePrimaryImage(long id, String imageId, String imageExtension) {
+        if (this.primaryImagePath == null) {
+            if (System.getProperty("os.name").startsWith("windows")) {
+                this.setPrimaryImage(String.format("business_%d\\%s%s", id, imageId, imageExtension));
+            } else {
+                this.setPrimaryImage(String.format("business_%d/%s%s", id, imageId, imageExtension));
+            }
+        }
+    }
+
 
 
     public String getPrimaryImagePath() {return this.primaryImagePath;}
